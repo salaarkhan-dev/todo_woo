ffcc7ca10f9c7165048c1923ade86a0aa437eb43
salaarkhan-dev <salaarkhan311@gmail.com>
2020-08-08 08:14:11 -0700

    Initial commit

--- manage.py
+++ manage.py
@@ -5,8 +5,7 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo_woo.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todowoo.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
